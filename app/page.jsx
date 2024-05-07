7ab8168af4d4b01ae09de057e370c896bea476f7
dogunfx <dogunfx@gmail.com>
2024-05-07 12:50:45 +0100

    made changes

---
+++ app/page.jsx
@@ -0,0 +1,34 @@
+export default function Page() {
+  return (
+    <div>
+      <button>Click Me</button>
+      <p>This will output the number of times the button was click</p>
+    </div>
+  );
+}
+
+function SimpleTodo() {}
+
+// comments are code or line of text that is ignore by language translator
+// comment are primary used fot explaining codes
+
+// the possible things that can be returned
+//1  ---- Javascript values
+//    Types of values
+// String values -----  alpha-numeric values
+// Numeric Values ---- are sequence of digit that are either in floating form or integer form
+// boolean  ---- values that can either be true or false
+
+// 2  --- Javascript Expression
+//     javascript expression are combination of values and operators that can evaluate to a single value
+
+// operators
+// arithmetic operators   -,+,*,/
+// relational operators      >,<, >=, <=, ==
+//conditional operators    ? :
+
+//3  --- HTML element / or group of HTML Elements
+
+// events ----- events are things that can happen to a component
+
+//events handlers are codes or javascripts statements that are executed when an events occurs
